8ebb527ab6febaf22082de781bc34f2bb3c558af
Neil Morrison <neil.morrison44@googlemail.com>
2015-02-12 23:05:24 +0000

    try half columns

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ camera.on("read", function(err, timestamp, filename){
     //     console.log("changed to png");
         
         //do stuff
-        var tube = pictureTube({cols: tty.WriteStream().columns});
+        var tube = pictureTube({cols: tty.WriteStream().columns/2});
         tube.pipe(process.stdout);
 
         var fs = require('fs');
