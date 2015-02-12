92e7d073cf02584a67f447e8ebcab07e5d24a5bf
Neil Morrison <neil.morrison44@googlemail.com>
2015-02-12 20:00:19 +0000

    First commit, might work?

---
+++ js/index.js
@@ -0,0 +1,37 @@
+var RaspiCam = require("raspicam");
+var pictureTube = require("picture-tube");
+
+var camera = new RaspiCam({mode:"photo", output:"image.png"});
+
+//to take a snapshot, start a timelapse or video recording
+camera.start( );
+
+//to stop a timelapse or video recording
+camera.stop( );
+
+//listen for the "start" event triggered when the start method has been successfully initiated
+camera.on("start", function(){
+    //do stuff
+});
+
+//listen for the "read" event triggered when each new photo/video is saved
+camera.on("read", function(err, timestamp, filename){ 
+    //do stuff
+    var tube = pictureTube();
+    tube.pipe(process.stdout);
+
+    var fs = require('fs');
+    fs.createReadStream('image.png').pipe(tube);
+
+
+});
+
+//listen for the "stop" event triggered when the stop method was called
+camera.on("stop", function(){
+    //do stuff
+});
+
+//listen for the process to exit when the timeout has been reached
+camera.on("exit", function(){
+    //do stuff
+});
